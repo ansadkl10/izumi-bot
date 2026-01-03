873f02441a7a0f42c75d6174dbf210ef5bdd935a
ansadkl10 <ansadanu95@gmail.com>
2026-01-03 21:36:12 +0530

    Initialize client and connect in index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+eo
+const client = require('./lib/client')
+require("./express.js");
+
+const connect = async () => {
+	try {
+		await client.initialize()
+	} catch (error) {
+		console.error(error)
+	}
+}
+
+connect()
